f027d39b5bc69d410d9855de65763f0ee6a29563
Hasibul Hasan <hshanto707@gmail.com>
2022-12-14 01:49:29 +0600

    7. Reverse Integer

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,13 @@
+// Problem URL: https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+public:
+  int reverse(int n) {
+    long long res = 0;
+    while(n) {
+      res = res * 10 + n % 10;
+      n /= 10;
+    }
+    return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+  }
+};
