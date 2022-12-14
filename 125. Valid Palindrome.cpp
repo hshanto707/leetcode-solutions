083434202ee768e0c2fa3f790f27ea67efe2ea35
Hasibul Hasan <hshanto707@gmail.com>
2022-12-14 16:52:19 +0600

    509. Fibonacci Number

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -2,21 +2,21 @@
 
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        vector <char> str1;
-        vector <char> str2;
-        
-        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9') {
-                str1.push_back(s[i]);
-                str2.push_back(s[i]);
-            }
-        }
-        
-        reverse(str2.begin(), str2.end());
-
-        return (str1 == str2);
+  bool isPalindrome(string s) {
+    vector <char> str1;
+    vector <char> str2;
+    
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9') {
+        str1.push_back(s[i]);
+        str2.push_back(s[i]);
+      }
     }
+    
+    reverse(str2.begin(), str2.end());
+
+    return (str1 == str2);
+  }
 };
--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -2,15 +2,15 @@
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        signed long long num = x;
-        signed long long res = 0;
-        while(num) {
-            res = res * 10 + num % 10;
-            num /= 10;
-        }
-
-        if (x == res && x >= 0) return true;
-        else return false;
+  bool isPalindrome(int x) {
+    signed long long num = x;
+    signed long long res = 0;
+    while(num) {
+      res = res * 10 + num % 10;
+      num /= 10;
     }
+
+    if (x == res && x >= 0) return true;
+    else return false;
+  }
 };
