4e9b2e94c77c9f4c7f90c34d85d42fc51c9c6444
Hasibul Hasan <hshanto707@gmail.com>
2022-12-21 18:16:11 +0600

    136. Single Number

---
+++ 136. Single Number.cpp
@@ -0,0 +1,12 @@
+// Problem URL: https://leetcode.com/problems/single-number
+
+class Solution {
+public:
+  int singleNumber(vector<int>& nums) {
+    int ans = 0;
+
+    for (auto num: nums) ans ^= num;
+
+    return ans;
+  }
+};
