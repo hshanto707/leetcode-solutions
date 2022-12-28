c596ee29fce7a7d966ef237d52409b4b423912e3
Hasibul Hasan <hshanto707@gmail.com>
2022-12-28 12:19:44 +0600

    169. Majority Element

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,19 @@
+// Problem URL: https://leetcode.com/problems/majority-element
+
+class Solution {
+public:
+  int majorityElement(vector<int>& nums) {
+    int ans = nums[0], cnt = 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+      if (cnt == 0) {
+        ans = nums[i];
+        cnt++;
+      }
+      else if (ans == nums[i]) cnt++;
+      else cnt--;
+    }
+    
+    return ans;
+  }
+};
