ea10e301a9ef1204ec352cc258c44f355d4b9480
hshanto707 <hshanto707@gmail.com>
2024-01-07 07:19:29 +0600

    2704. To Be Or Not To Be

---
+++ 30 Days of JavaScript/2704. To Be Or Not To Be.ts
@@ -0,0 +1,17 @@
+type ToBeOrNotToBe = {
+  toBe: (val: any) => boolean;
+  notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+  return {
+    toBe: (val2: any): boolean => {
+      if (val !== val2) throw new Error("Not Equal");
+      return true;
+    },
+    notToBe: (val2: any): boolean => {
+      if (val === val2) throw new Error("Equal");
+      return true;
+    },
+  };
+}
